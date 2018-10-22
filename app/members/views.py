fbf13ac50579e312b413703257fb339df79b680a
ksw <zenuine1@gmail.com>
2018-10-22 17:09:22 +0900

    signup view에서 중복 render()부분 제거

--- app/members/views.py
+++ app/members/views.py
@@ -29,31 +29,22 @@ def logout_view(request):
 
 
 def signup_view(request):
+    context = {
+        'form': SignupForm(),
+    }
     if request.method == 'POST':
         username = request.POST['username']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
+
         if User.objects.filter(username=username).exists():
-            form = SignupForm()
-            context = {
-                'form': form,
-                'error': f'사용자명({username})은 이미 사용중입니다.'
-            }
+            context['error'] = f'사용자명({username})은 이미 사용중입니다.'
             return render(request, 'members/signup.html', context)
-            # return HttpResponse(f'({username})은 이미 사용중입니다')
-        if password1 != password2:
-            form = SignupForm()
-            context = {
-                'form': form,
-                'error': f'사용자명({username})은 이미 사용중입니다.'
-            }
+        elif password1 != password2:
+            context['error'] = f'사용자명({username})은 이미 사용중입니다.'
             return render(request, 'members/signup.html', context)
-            # return HttpResponse(f'비밀번호와 비밀번호 확인란의 값이 일치하지 않습니다.')
-        user = User.objects.create_user(username=username, password=password1)
-        login(request, user)
-        return redirect('posts:post-list')
-    form = SignupForm()
-    context = {
-        'form': form,
-    }
+        else:
+            user = User.objects.create_user(username=username, password=password1,)
+            login(request, user)
+            return redirect('posts:post-list')
     return render(request, 'members/signup.html', context)
