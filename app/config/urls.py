b5408e35bf63a6160d9ebaa9b0410c1e7e264d95
ksw <zenuine1@gmail.com>
2018-10-23 11:05:38 +0900

    RedirectView사용

--- app/config/urls.py
+++ app/config/urls.py
@@ -17,9 +17,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('', views.index, name='index'),
+    path('', RedirectView.as_view(pattern_name='posts:post-list'), name='index'),
     path('posts/', include('posts.urls')),
     path('members/', include('members.urls')),
 ]
