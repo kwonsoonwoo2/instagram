d2b9fed30d9f02754dac5245294b1883de372cb8
ksw <zenuine1@gmail.com>
2018-10-29 14:33:40 +0900

    좋아요 처리 후 해당 Post위치로 이동

--- app/posts/models.py
+++ app/posts/models.py
@@ -6,15 +6,27 @@ from django.db import models
 
 class Post(models.Model):
     author = models.ForeignKey(
-        # <AppName>.<ModelName>
-        # 'members.User',
+        # 'auth.User'
+        #  Django가 기본으로 제공하는 User클래스
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         verbose_name='작성자',
     )
-    photo = models.ImageField(verbose_name='사진', upload_to='post')
+    photo = models.ImageField(
+        '사진',
+        upload_to='post',
+    )
+    # auto_now_add: 객체가 처음 생성될때의 시간 저장
+    # auto_now: 객체의 save()가 호출될 때 마다 시간 저장
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        through='PostLike',
+        related_name='like_posts',
+        related_query_name='like_post',
+    )
 
     class Meta:
         verbose_name = '포스트'
@@ -22,7 +34,8 @@ class Post(models.Model):
         ordering = ['-pk']
 
     def like_toggle(self, user):
-        pass
+        # 전달받은 user가 이 Post를 Like한다면 해제
+        # 안되어있다면 Like처리
         postlike, postlike_created = self.postlike_set.get_or_create(user=user)
         if not postlike_created:
             postlike.delete()
@@ -40,7 +53,6 @@ class Comment(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         verbose_name='작성자',
-
     )
     content = models.TextField('댓글 내용')
     tags = models.ManyToManyField(
@@ -58,18 +70,17 @@ class Comment(models.Model):
 
     def save(self, *args, **kwargs):
         def save_html():
+            # 저장하기 전에 _html필드를 채워야 함 (content값을 사용해서)
             self._html = re.sub(
                 self.TAG_PATTERN,
                 r'<a href="/explore/tags/\g<tag>/">#\g<tag></a>',
                 self.content,
             )
 
-        # 저장하기 전에 _html필드를 채워야 함 (content값을 사용해서)
-
         def save_tags():
             # DB에 Comment저장이 완료된 후,
-            #   자신의 'content'값에서 해시태그 목록을 가져와서
-            #   자신의 'tags"속성 (MTM필드)에 할당
+            #  자신의 'content'값에서 해시태그 목록을 가져와서
+            #  자신의 'tags'속성 (MTM필드)에 할당
             tags = [HashTag.objects.get_or_create(name=name)[0]
                     for name in re.findall(self.TAG_PATTERN, self.content)]
             self.tags.set(tags)
@@ -85,13 +96,10 @@ class Comment(models.Model):
         #  아래와 같이 변경
         #   <a href="/explore/tags/{태그명}>/">#{태그명}</a>
         # re.sub를 사용
-        re.sub(
-            self.TAG_PATTERN,
-            r'<a href="/explore/tags/\g<tag>/">#\g<tag></a>',
-            self.content,
-        )
+        return self._html
 
         # 템플릿에서는 comment.content대신 comment.html을 출력
+
         # 숙제
         #  /explore/tags/{태그명}/ URL에서
         #  해당 태그를 가진 Post목록을 보여주는 view, url, template구현
@@ -100,9 +108,9 @@ class Comment(models.Model):
         #       tag_post_list(request, tag_name)
         #  template:
         #       /posts/tag_post_list.html
+
         # base.html에 있는 검색창에 값을 입력하고 Enter시 (Submit)
         #  해당 값을 사용해 위에서 만든 view로 이동
-        return self._html
 
 
 class HashTag(models.Model):
@@ -118,3 +126,27 @@ class HashTag(models.Model):
     class Meta:
         verbose_name = '해시태그'
         verbose_name_plural = f'{verbose_name} 목록'
+
+
+class PostLike(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 'Post[{post_pk}] Like (User: {username})'.format(
+            post_pk=self.post.pk,
+            username=self.user.username,
+        )
+
+    class Meta:
+        # 특정 User가 특정 Post를 좋아요 누른 정보는 unique해야함
+        unique_together = (
+            ('post', 'user'),
+        )
--- app/posts/views.py
+++ app/posts/views.py
@@ -2,26 +2,41 @@ import re
 
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 
 from .forms import CommentForm, PostForm
 from .models import Post
 
 
 def post_list(request):
-    # Post모델에
-    #   created_at (생성시간 저장)
-    #   modified_at (수정시간 저장)
-    #       두 필드를 추가
+    # 1. Post모델에
+    #  created_at (생성시간 저장)
+    #       auto_now_add=True
+    #  modified_at (수정시간 저장)
+    #       auto_now=True
+    #   두 필드를 추가
 
-    # 2. Post모델이 기본적으로 pk 내림차순으로 정렬되도록 설정
+    # 2. Post모델이 기본적으로 pk내림차순으로 정렬되도록 설정
+    #     class Meta:
+    #         ordering = ['-pk']
 
     # 3. 모든 Post객체에 대한 QuerySet을
-    #   render의 context인수로 전달
+    #    render의 context인수로 전달 (키: posts)
+    #      context = {'posts': Post.objects.all()}
 
     # 4. posts/post_list.html을 Template으로 사용
-    #   템플릿에서는 posts값을 순회하며
-    #   각 Post의 photo정보를 출력
+    #     템플릿에서는 posts값을 순회하며
+    #     각 Post의 photo정보를 출력
+
+    # 5. url은 posts.urls모듈을 사용
+    #    config.urls에서 해당 모듈을 include
+    #    posts.urls.app_name = 'posts'를 사용
+    #     include할 URL은 'posts/'
+    #     view의 URL은 비워둔다
+    #      결과: localhost:8000/posts/ 로 접근시
+    #            이 view가 처리하도록 함
     posts = Post.objects.all()
+    # 적절히 CommentCreateForm을 전달
     context = {
         'posts': posts,
         'comment_form': CommentForm(),
@@ -33,13 +48,19 @@ def post_list(request):
 def post_create(request):
     context = {}
     if request.method == 'POST':
+        # request.FILES에 form에서 보낸 파일객체가 들어있음
+        # 새로운 Post를 생성한다.
+        #  author는 User.objects.first()
+        #  photo는 request.FILES에 있는 내용을 적절히 꺼내서 쓴다
+        # 완료된 후 posts:post-list로 redirect
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.save()
 
-            # 위에서 생성한 Post에 연결되는 Comment생성
+            # PostForm에 'comment'값이 전달되었다면
+            # 위에서 만든 Post와 연결되는 Comment를 생성
             comment_content = form.cleaned_data['comment']
             if comment_content:
                 post.comments.create(
@@ -48,6 +69,8 @@ def post_create(request):
                 )
             return redirect('posts:post-list')
     else:
+        # GET요청의 경우, 빈 Form인스턴스를 context에 담아서 전달
+        # Template에서는 'form'키로 해당 Form인스턴스 속성을 사용 가능
         form = PostForm()
 
     context['form'] = form
@@ -55,6 +78,23 @@ def post_create(request):
 
 
 def comment_create(request, post_pk):
+    """
+    post_pk에 해당하는 Post에 댓글을 생성하는 view
+    'POST'메서드 요청만 처리
+    'content'키로 들어온 값을 사용해 댓글 생성. 작성자는 요청한 User
+    URL: /posts/<post_pk>/comments/create/
+    댓글 생성 완료 후에는 posts:post-list로 redirect
+    :param request:
+    :param post_pk:
+    :return:
+    """
+    # 1. post_pk에 해당하는 Post객체를 가져와 post변수에 할당
+    # 2. request.POST에 전달된 'content'키의 값을 content변수에 할당
+    # 3. Comment생성
+    #     author: 현재 요청의 User
+    #     post: post_pk에 해당하는 Post객체
+    #     content: request.POST로 전달된 'content'키의 값
+    # 4. posts:post-list로 redirect하기
     if request.method == 'POST':
         post = Post.objects.get(pk=post_pk)
         form = CommentForm(request.POST)
@@ -63,14 +103,16 @@ def comment_create(request, post_pk):
             comment.post = post
             comment.author = request.user
             comment.save()
-
             return redirect('posts:post-list')
 
 
 def tag_post_list(request, tag_name):
+    # Post중, 자신에게 속한 Comment가 가진 HashTag목록 중 tag_name이 name인 HashTag가 포함된
+    #  Post목록을 posts변수에 할당
+    #  context에 담아서 리턴 render
+    # HTML: /posts/tag_post_list.html
     posts = Post.objects.filter(
-        comments__tags__name=tag_name
-    )
+        comments__tags__name=tag_name).distinct()
     context = {
         'posts': posts,
     }
@@ -78,13 +120,26 @@ def tag_post_list(request, tag_name):
 
 
 def tag_search(request):
+    # request.GET으로 전달된
+    #  search_keyword값을 적절히 활용해서
+    #  위의 tag_post_list view로 redirect
+    # URL: '/posts/tag-search/'
+    # URL Name: 'posts:tag-search'
+    # Template: 없음
     search_keyword = request.GET.get('search_keyword')
-    substitute_keyword = re.sub(r'#|\s', '', search_keyword)
-    return redirect('tag-post-list', substitute_keyword)
+    substituted_keyword = re.sub(r'#|\s+', '', search_keyword)
+    return redirect('tag-post-list', substituted_keyword)
 
 
 def post_like_toggle(request, post_pk):
+    # URL: '/posts/<post_pk>/like-toggle/
+    # URL Name: 'posts:post-like-toggle'
+    # POST method에 대해서만 처리
+
+    # request.user가 post_pk에 해당하는 Post에
+    #  Like Toggle처리
     if request.method == 'POST':
         post = get_object_or_404(Post, pk=post_pk)
         post.like_toggle(request.user)
-        return redirect('posts:post-list')
+        url = reverse('posts:post-list')
+        return redirect(url + f'#post-{post_pk}')
